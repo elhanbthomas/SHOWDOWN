e3e53b6cda526588dfea63363bd1cafe0b723402
elhanbthomas <elhanbthomas@gmail.com>
2024-12-23 12:56:22 +0530

    edit profile

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,4 +4,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/',include('individual.urls')),
+    path('api/',include('hospital.urls')),
 ]
