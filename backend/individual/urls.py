2ad2cafa3f4b22050bebbd724abb92340d6fe656
elhanbthomas <elhanbthomas@gmail.com>
2024-12-26 20:19:21 +0530

    registration

--- backend/individual/urls.py
+++ backend/individual/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import edit_profile
+from .views import registerIndividual, edit_profile
 urlpatterns = [
+    path('individual/register', registerIndividual, name='individual_registration'),
     path('profile/edit', edit_profile, name='edit_profile'),
 ]
