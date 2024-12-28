034205d624e324c027cfb528dd527a33da39b4d1
Aditya Karn <adityakarn0001@gmail.com>
2024-12-28 15:07:49 +0530

    routing

--- frontend_homepage/src/main.jsx
+++ frontend_homepage/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import {Route,createBrowserRouter,createRoutesFromElements,BrowserRouter,RouterProvider } from "react-router";
+
 import Report from './components/statistics/report'
 import States from './components/statistics/state'
 import Stats from './components/statistics/stats'
@@ -8,8 +10,25 @@ import App from './App.jsx'
 import Heart from './components/organs/heart'
 import Donor_home from './components/register_donor_home/donor_home'
 import Approved_hospital from './components/approved_hospital/app_hosp'
+
+
+const router=createBrowserRouter(
+
+createRoutesFromElements(
+
+<Route path='/' element={<App/>}>
+
+  </Route>
+
+)
+
+
+)
+
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App/>
+     <RouterProvider router={router}/>
   </StrictMode>,
 )
